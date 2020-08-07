9193de6b5ba336bf99bf291ab1d7a2b26bff4011
Ehsan <Ehsan@MacBook-Pro.local>
2020-08-07 12:12:43 +0430

    Add databases submission, and another algorithm solution

--- algorithms/src/TwoSum.kt
+++ algorithms/src/TwoSum.kt
@@ -1,15 +1,15 @@
 fun main() {
     val twoSum = TwoSum()
     val array = intArrayOf(2, 7, 11, 15)
-    val result = twoSum.twoSum(array, 9)
+    val result = twoSum.run(array, 9)
     for (value in result) {
         print(value)
     }
 }
 
 class TwoSum {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
 
+    fun run(nums: IntArray, target: Int): IntArray {
         val map = HashMap<Int, Int>()
         val indices = IntArray(2)
 
