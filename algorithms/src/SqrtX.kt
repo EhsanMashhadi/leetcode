0476386294950538af063db84f5831a4fff8ece7
Ehsan <Ehsan@MacBook-Pro.local>
2020-08-13 10:25:29 +0430

    Add SqrtX solution

---
+++ algorithms/src/SqrtX.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val sqrt = SqrtX()
+    val result = sqrt.run(9)
+    println(result)
+}
+
+class SqrtX {
+
+    fun run(x: Int): Int {
+        if (x == 0) {
+            return 0
+        }
+        var lastValue = 1
+        for (i in 1..(x / 2) + 1) {
+            val sum = i * i
+            when {
+                sum == x -> {
+                    return i
+                }
+                sum < 0 || sum > x -> {
+                    return lastValue
+                }
+                else -> {
+                    lastValue = i
+                }
+            }
+        }
+        return lastValue
+    }
+}
