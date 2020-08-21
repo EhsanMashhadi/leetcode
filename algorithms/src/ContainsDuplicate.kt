68dcfbfbbc06ab85fa5efd588ca4e86e6ef3d0f6
Ehsan <Ehsan@MacBook-Pro.local>
2020-08-21 16:55:12 +0430

    Add ContainsDuplicate solution

---
+++ algorithms/src/ContainsDuplicate.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val containsDuplicate = ContainsDuplicate()
+    val result = containsDuplicate.run(intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2))
+    println(result)
+}
+
+class ContainsDuplicate {
+
+    fun run(nums: IntArray): Boolean {
+        val hashSet = HashSet<Int>()
+        for (num in nums) {
+            if (!hashSet.add(num))
+                return true
+        }
+        return false
+    }
+}
