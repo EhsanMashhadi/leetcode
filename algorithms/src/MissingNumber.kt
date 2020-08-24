b22b6d12a72e62b688879f7c5ff5a28867cf55f9
Ehsan <Ehsan@MacBook-Pro.local>
2020-08-24 11:53:12 +0430

    Add MissingNumber solution

---
+++ algorithms/src/MissingNumber.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val missingNumber = MissingNumber()
+    val result = missingNumber.run(intArrayOf(0,1))
+    println(result)
+}
+
+class MissingNumber {
+
+    fun run(nums: IntArray): Int {
+        val expectedSum = (nums.size * (nums.size + 1)) / 2
+        val sum = nums.sum()
+        return expectedSum - sum
+    }
+}
